2bc4f9944d6fc4d49bae3e68ea13a25a2f715ed6
Allkman <algirdas.cern@gmail.com>
2021-04-26 20:21:22 +0300

    init comit

---
+++ Kartojimas/BaseClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kartojimas
+{
+    abstract class BaseClass
+    {
+        protected BaseClass(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+        public abstract string NameMetodas();
+
+        public abstract int NameKodas(int asmKodas);
+
+    }
+}
