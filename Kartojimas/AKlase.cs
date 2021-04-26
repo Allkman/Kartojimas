cb57f404e3124fc3746ae6ba38c8eb45eca3989f
Allkman <algirdas.cern@gmail.com>
2021-04-26 20:50:33 +0300

    NameMetodas();

---
+++ Kartojimas/AKlase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kartojimas
+{
+    public class AKlase : BaseClass
+    {
+        public AKlase(string name): base(name)
+        {
+
+        }
+        public override string NameMetodas()
+        {
+            var sb = new StringBuilder();
+            List<char> visosBalses = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+            foreach (var raide in Name)
+            {
+                sb.Append(visosBalses.Contains(raide) ? (int)raide : raide);
+            }
+            return sb.ToString();
+
+        }
+        public override int NameKodas(int asmKodas)
+        {
+            
+        }
+
+
+    }
+}
--- Kartojimas/BaseClass.cs
+++ Kartojimas/BaseClass.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Kartojimas
 {
-    abstract class BaseClass
+    public abstract class BaseClass
     {
         protected BaseClass(string name)
         {
