b93151cd4856610a9c8e7edf5aecd8fc3a3b7d79
Allkman <algirdas.cern@gmail.com>
2021-04-28 20:31:23 +0300

    Kamuolys +

---
+++ Kartojimas/Kamuolys.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kartojimas
+{
+    public class Kamuolys
+    {
+        Spalva color = new Spalva();
+        private int ballSize = 7;
+        private int throwCount = 0;
+        public Kamuolys()
+        {
+
+        }
+        public void Mesti()
+        {
+            throwCount += 1;
+        }
+        public void Susprogdinti()
+        {
+            ballSize = 0;
+        }
+    }
+}
